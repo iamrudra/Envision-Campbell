32123009318bfdf80041fe724544571f15e81946
iamrudra <vidit.maniyar@gmail.com>
2015-10-04 10:14:42 -0700

    Added ajax call to the search frontend

--- routes/browsing.js
+++ routes/browsing.js
@@ -6,4 +6,9 @@ router.get('/browsing', function(req, res, next) {
 	  res.render('browsing', { title: 'Admin' });
 });
 
+router.post('/browsing', function(req, res, next) {
+	console.log('body: ' + JSON.stringify(req.body));
+	//res.redirect('/browsing');
+});
+
 module.exports = router;
